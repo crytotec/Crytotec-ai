a3d1292bd4ffc0af2fcb8bca75002761ec048eb3
oladimeji seyi <seyioladimeji790@gmail.com>
2026-06-23 21:21:32 +0100

    fix typescript errors

--- Frontend/src/Page/Login.tsx
+++ Frontend/src/Page/Login.tsx
@@ -6,6 +6,7 @@ import toast from "react-hot-toast";
 import { useNavigate, Link } from "react-router-dom";
 
 function Login() {
+  the auth
   const { login } = useAuth();
   const navigate = useNavigate();
 
